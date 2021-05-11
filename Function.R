80fcb18685c530d320210ff1d91367b3be2ed515
Ballisticdom <82590739+Ballisticdom@users.noreply.github.com>
2021-05-11 10:32:32 -0400

    Update Function.R

--- Function.R
+++ Function.R
@@ -2,37 +2,37 @@ CorTestNonZero <- function(rho0,x,y,tailed=c("right","left","two"))
 {
   r12 <- cor(x,y,method = "pearson")
   n <- length(x)
-  z0 <- (0.5 * log((1+r12)/(1-r12)) - 0.5 * log((1+rho0)/(1-rho0))) / (1 / (n-3)^0.5)
+  t0 <- (0.5 * log((1+r12)/(1-r12)) - 0.5 * log((1+rho0)/(1-rho0))) / (1 / (n-3)^0.5)
   if(tailed == "left")
   {
     Setup <- c("Left Tailed Test")
-    Tstat <- z0
+    Tstat <- t0
     DF <- n-2
-    pValue <- pt(z0)
+    pValue <- pt(t0)
     CorrelationCoefficient <- r12
     results <- data.frame(Setup,Tstat,DF,pValue,CorrelationCoefficient)
   }
   if(tailed == "right")
   {
     Setup <- c("Right Tailed Test")
-    Tstat <- z0
+    Tstat <- t0
     DF <- n-2
-    pValue <- 1-pt(z0)
+    pValue <- 1-pt(t0)
     CorrelationCoefficient <- r12
     results <- data.frame(Setup,Tstat,DF,pValue,CorrelationCoefficient)
   }  
   if(tailed ==  "two")
   {
     Setup <- c("Two Tailed Test")
-    Tstat <- z0
+    Tstat <- t0
     DF <- n-2
-    if(z0 > 0)
+    if(t0 > 0)
     {
-      pValue <- 2*(1-pt(z0))
+      pValue <- 2*(1-pt(t0))
     }
-    if(z0 < 0)
+    if(t0 < 0)
     {
-      pValue <- 2*pt(z0)
+      pValue <- 2*pt(t0)
     }
     CorrelationCoefficient <- r12
     results <- data.frame(Setup,Tstat,DF,pValue,CorrelationCoefficient)
